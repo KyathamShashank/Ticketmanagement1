a5c21af798f14d5892bf3ea4dab83798f5414122
Shashank <kyathamshashank1723@gmail.com>
2024-10-24 16:05:19 +0530

    first commit

---
+++ TicketManagement1/src/main/java/com/nt/TicketManagement1Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManagement1Application.class, args);
+	}
+
+}
